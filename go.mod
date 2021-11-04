07eb32a7ea72a9ad0f38fc31d76445d8d0a6d52e
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-11-05 01:57:08 +0530

    cors

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.4
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/itsjamie/gin-cors v0.0.0-20160420130702-97b4a9da7933 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/lib/pq v1.10.3 // indirect
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/44t4nk1/StudentPortal/api/routes"
+	cors "github.com/itsjamie/gin-cors"
 
 	"github.com/44t4nk1/StudentPortal/api/db"
 	"github.com/gin-gonic/gin"
@@ -20,6 +22,18 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	router.Use(
+		cors.Middleware(
+			cors.Config{
+				Origins:         "*",
+				Methods:         "GET, PUT, POST, DELETE",
+				RequestHeaders:  "Origin, Authorization, Content-Type",
+				ExposedHeaders:  "",
+				MaxAge:          50 * time.Second,
+				Credentials:     true,
+				ValidateHeaders: false,
+			}))
 
 	routes.InitRoutes(router)
 }
